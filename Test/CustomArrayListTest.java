b346080cb52ac65e9fa938a375c52bfb6e798f52
MiriamLaw <mimsical999@yahoo.com>
2023-10-29 13:01:41 -0700

    Updated test cases with Objects instead of just Strings

--- Test/CustomArrayListTest.java
+++ Test/CustomArrayListTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -13,107 +14,93 @@ public class CustomArrayListTest {
 	@Test
 	public void test_Add() {
 
-		CustomArrayList<String> customArrayList = new CustomArrayList<>();
+		CustomArrayList<Object> customArrayList = new CustomArrayList<>();
 
-		boolean result = customArrayList.add("Test Item");
-
-		assertTrue(result);
-
+		assertTrue(customArrayList.add("Test Item"));
 		assertEquals("Test Item", customArrayList.get(0));
 	}
 
 	@Test
 	public void test_Add_To_Empty_List() {
 
-		CustomList<String> customList = new CustomArrayList<>();
+		CustomList<Object> customList = new CustomArrayList<>();
 
 		assertEquals(0, customList.getSize());
-
-		boolean added = customList.add("Item1");
-
-		assertTrue(added);
+		assertTrue(customList.add("Item1"));
 		assertEquals(1, customList.getSize());
+		assertTrue(customList.get(0) instanceof String);
+		assertEquals("Item1", customList.get(0));
 
 	}
 
 	@Test
 	public void test_Add_At_Index() {
 
-		CustomList<String> customList = new CustomArrayList<>();
+		CustomList<Object> customList = new CustomArrayList<>();
 
 		customList.add("Item2");
 		customList.add("Item3");
 
 		assertEquals(2, customList.getSize());
-		assertEquals("Item2", customList.get(0));
-		assertEquals("Item3", customList.get(1));
-
-		boolean added = customList.add(0, "Item1");
-
-		assertTrue(added);
+		assertTrue(customList.add(0, "Item1"));
 		assertEquals(3, customList.getSize());
-
+		assertTrue(customList.get(0) instanceof String);
 		assertEquals("Item1", customList.get(0));
+		assertTrue(customList.get(1) instanceof String);
 		assertEquals("Item2", customList.get(1));
+		assertTrue(customList.get(2) instanceof String);
 		assertEquals("Item3", customList.get(2));
+
 	}
 
 	@Test
 	public void test_Add_Item_To_End_Of_List() {
-		CustomList<String> customList = new CustomArrayList<>();
+		CustomList<Object> customList = new CustomArrayList<>();
 
 		customList.add("Item1");
 		customList.add("Item2");
 
 		assertEquals(2, customList.getSize());
-
-		boolean added = customList.add("Item3");
-
-		assertTrue(added);
+		assertTrue(customList.add("Item3"));
 		assertEquals(3, customList.getSize());
+		assertTrue(customList.get(2) instanceof String);
 		assertEquals("Item3", customList.get(2));
 	}
 
 	@Test
 	public void test_Add_Item_At_Specific_Index() {
-		CustomList<String> customList = new CustomArrayList<>();
+		CustomList<Object> customList = new CustomArrayList<>();
 
 		customList.add("Item1");
 		customList.add("Item2");
 
 		assertEquals(2, customList.getSize());
-
-		boolean added = customList.add(1, "Item3");
-
-		assertTrue(added);
+		assertTrue(customList.add(1, "Item3"));
 		assertEquals(3, customList.getSize());
+		assertTrue(customList.get(0) instanceof String);
 		assertEquals("Item1", customList.get(0));
+		assertTrue(customList.get(1) instanceof String);
 		assertEquals("Item3", customList.get(1));
+		assertTrue(customList.get(2) instanceof String);
 		assertEquals("Item2", customList.get(2));
 	}
 
 	@Test
 	public void test_Add_At_OutOfBoundsIndex() {
-		CustomList<String> customList = new CustomArrayList<>();
+		CustomList<Object> customList = new CustomArrayList<>();
 
 		customList.add("Item1");
 		customList.add("Item2");
 
 		assertEquals(2, customList.getSize());
-
-		try {
-			customList.add(5, "Item3");
-			fail("Expected IndexOutOfBoundsException but it wasn't thrown");
-		} catch (IndexOutOfBoundsException e) {
-
-			assertEquals(2, customList.getSize());
-		}
+		assertThrows(IndexOutOfBoundsException.class, () -> customList.add(5, "Item3"));
+		assertEquals(2, customList.getSize());
 
 	}
 
 	@Test
 	public void test_Add_Multiple_Items_At_Various_Positions() {
-		CustomList<String> customList = new CustomArrayList<>();
+		CustomList<Object> customList = new CustomArrayList<>();
 
 		customList.add("Item1");
 		assertEquals(1, customList.getSize());
@@ -133,45 +120,46 @@ public class CustomArrayListTest {
 
 	@Test
 	public void test_Add_To_Full_List() {
-		CustomList<String> customList = new CustomArrayList<>();
+		CustomList<Object> customList = new CustomArrayList<>();
 
 		for (int i = 1; i <= 10; i++) {
 			customList.add("Item" + i);
 		}
 
 		assertEquals(10, customList.getSize());
-
 		customList.add("Item11");
-
 		assertEquals(11, customList.getSize());
 		assertEquals("Item11", customList.get(10));
 	}
 
 	@Test
 	public void test_Add_Null_Item() {
-		CustomList<String> customList = new CustomArrayList<>();
+		CustomList<Object> customList = new CustomArrayList<>();
 
 		boolean added = customList.add(null);
 
 		assertTrue(added);
 		assertEquals(1, customList.getSize());
-
 		assertNull(customList.get(0));
 	}
 
 	@Test
 	public void test_Remove() {
-		CustomList<String> customList = new CustomArrayList<>();
+		CustomList<Object> customList = new CustomArrayList<>();
 
 		customList.add("Item1");
 		customList.add("Item2");
 		customList.add("Item3");
 
-		String removedItem = customList.remove(1);
+		assertTrue(customList.get(1) instanceof String);
+
+		String removedItem = (String) customList.remove(1);
 
 		assertEquals("Item2", removedItem);
 		assertEquals(2, customList.getSize());
+		assertTrue(customList.get(0) instanceof String);
 		assertEquals("Item1", customList.get(0));
+		assertTrue(customList.get(1) instanceof String);
 		assertEquals("Item3", customList.get(1));
 
 	}
@@ -179,7 +167,7 @@ public class CustomArrayListTest {
 	@Test
 	public void test_Remove_From_Empty_List() {
 
-		CustomList<String> customList = new CustomArrayList<>();
+		CustomList<Object> customList = new CustomArrayList<>();
 
 		try {
 			customList.remove(0);
@@ -197,13 +185,13 @@ public class CustomArrayListTest {
 	@Test
 	public void test_Remove_From_Beginning() {
 
-		CustomList<String> customList = new CustomArrayList<>();
+		CustomList<Object> customList = new CustomArrayList<>();
 
 		customList.add("Item1");
 		customList.add("Item2");
 		customList.add("Item3");
 
-		String removedItem = customList.remove(0);
+		String removedItem = (String) customList.remove(0);
 
 		assertEquals("Item1", removedItem);
 		assertEquals(2, customList.getSize());
@@ -213,13 +201,13 @@ public class CustomArrayListTest {
 
 	@Test
 	public void test_Remove_From_End() {
-		CustomList<String> customList = new CustomArrayList<>();
+		CustomList<Object> customList = new CustomArrayList<>();
 
 		customList.add("Item1");
 		customList.add("Item2");
 		customList.add("Item3");
 
-		String removedItem = customList.remove(2);
+		String removedItem = (String) customList.remove(2);
 
 		assertEquals("Item3", removedItem);
 		assertEquals(2, customList.getSize());
@@ -230,13 +218,13 @@ public class CustomArrayListTest {
 
 	@Test
 	public void test_Remove_From_Middle() {
-		CustomList<String> customList = new CustomArrayList<>();
+		CustomList<Object> customList = new CustomArrayList<>();
 
 		customList.add("Item1");
 		customList.add("Item2");
 		customList.add("Item3");
 
-		String removedItem = customList.remove(1);
+		String removedItem = (String) customList.remove(1);
 
 		assertEquals("Item2", removedItem);
 		assertEquals(2, customList.getSize());
@@ -246,11 +234,12 @@ public class CustomArrayListTest {
 
 	@Test
 	public void test_Remove_Last_Item() {
-		CustomList<String> customList = new CustomArrayList<>();
+		CustomList<Object> customList = new CustomArrayList<>();
 
 		customList.add("Item1");
+		assertTrue(customList.get(0) instanceof String);
 
-		String removedItem = customList.remove(0);
+		String removedItem = (String) customList.remove(0);
 
 		assertEquals("Item1", removedItem);
 		assertEquals(0, customList.getSize());
--- src/com/coderscampus/assignment7/CustomArrayList.java
+++ src/com/coderscampus/assignment7/CustomArrayList.java
@@ -1,23 +1,14 @@
 package com.coderscampus.assignment7;
 
-import java.util.Arrays;
-
 public class CustomArrayList<T> implements CustomList<T> {
-	private Object[] items = new Object[10];
-	private int size = 0;
-	private Class<T> clazz;
+	private Object[] items;
+	private int size;
 
-	public CustomArrayList(Class<T> clazz) {
-		this.clazz = clazz;
-	}
-
-	@SuppressWarnings("unchecked")
 	public CustomArrayList() {
-		this.clazz = (Class<T>) String.class;
-
+		items = new Object[10];
+		size = 0;
 	}
 
-	@Override
 	public boolean add(T item) {
 		increaseArraySizeIfNeeded();
 		items[size] = item;
@@ -25,7 +16,6 @@ public class CustomArrayList<T> implements CustomList<T> {
 		return true;
 	}
 
-	@Override
 	public boolean add(int index, T item) throws IndexOutOfBoundsException {
 		if (index < 0 || index > size) {
 			throw new IndexOutOfBoundsException("Index is out of bounds");
@@ -33,6 +23,12 @@ public class CustomArrayList<T> implements CustomList<T> {
 
 		if (size == items.length) {
 			increaseArraySizeIfNeeded();
+		}
+
+		if (index == size) {
+			items[size] = item;
+			size++;
+			return true;
 		}
 
 		for (int i = size; i > index; i--) {
@@ -44,46 +40,41 @@ public class CustomArrayList<T> implements CustomList<T> {
 		return true;
 	}
 
-	@Override
 	public int getSize() {
 		return size;
 	}
 
-	@Override
+	@SuppressWarnings("unchecked")
 	public T get(int index) throws IndexOutOfBoundsException {
 		if (index < 0 || index >= size) {
 			throw new IndexOutOfBoundsException("Index is out of bounds");
-
 		}
-		@SuppressWarnings("unchecked")
-		T element = (T) items[index];
-		return element;
+
+		return (T) items[index];
 	}
 
-	private void increaseArraySizeIfNeeded() {
-		if (size == items.length) {
-			Object[] newItems = Arrays.copyOf(items, items.length * 2);
-			items = newItems;
-		}
-	}
-
-	@Override
 	public T remove(int index) throws IndexOutOfBoundsException {
 		if (index < 0 || index >= size) {
 			throw new IndexOutOfBoundsException("Index is out of bounds");
 		}
 
-		if (items[index] == null) {
-			throw new NullPointerException("Item at index is null");
-		}
-
-		T removedItem = clazz.cast(items[index]);
+		@SuppressWarnings("unchecked")
+		T removedItem = (T) items[index];
 
 		for (int i = index; i < size - 1; i++) {
 			items[i] = items[i + 1];
 		}
+
+		items[size - 1] = null; // Clear the last element
 		size--;
 		return removedItem;
 	}
 
+	private void increaseArraySizeIfNeeded() {
+		if (size >= items.length) {
+			Object[] newArray = new Object[size * 2];
+			System.arraycopy(items, 0, newArray, 0, size);
+			items = newArray;
+		}
+	}
 }
